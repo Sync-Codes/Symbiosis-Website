26e6d86599ff20150df9d5598c74fb3a55af7b48
Saransh <saranshlingwal923@gmail.com>
2023-02-21 15:53:01 +0530

    added close menu in togglemenu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = () => {
         const toggleMenu = document.querySelector('.toggleMenu');
         const navigation = document.querySelector('.navigation');
+
         toggleMenu.classList.toggle('active');
         navigation.classList.toggle('active');
+
+        if (toggleMenu.classList.contains("active")) {
+            toggleMenu.style.backgroundImage = "url(/images/close.png)";
+        } else {
+            toggleMenu.style.backgroundImage = "url(/images/menu.png)";
+        }
     }
 
     document.querySelector('.toggleMenu').addEventListener('click', menuToggle);
