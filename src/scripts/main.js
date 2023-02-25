e934ac4303d68d16f2c03c493f4ed0f94f46a0f5
Saransh <saranshlingwal923@gmail.com>
2023-02-25 17:35:30 +0530

    add active navlink color for active pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = () => {
         const toggleMenu = document.querySelector('.toggleMenu');
         const navigation = document.querySelector('.navigation');
+        const socialtoggle = document.querySelector('.socialtoggle');
 
         toggleMenu.classList.toggle('active');
         navigation.classList.toggle('active');
-
+        socialtoggle.classList.toggle('active');
         // if (toggleMenu.classList.contains("active")) {
         //     toggleMenu.style.backgroundImage = "url(/images/close.png)";
         // } else {
@@ -15,3 +16,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.querySelector('.toggleMenu').addEventListener('click', menuToggle);
   });
+
+
+
+const navLinkEles = document.querySelectorAll('.nav__link');
+const windowPathname = window.location.pathname;
+
+navLinkEles.forEach(navlinkEle =>{
+    const navLinkPathname = new URL(navlinkEle.href).pathname;
+
+
+    if(windowPathname === navLinkPathname){
+        navlinkEle.classList.add('active');
+    }
+});
